f165c580acae2d7a89b6754347e2907d9de2cb21
hamilkarr <12uclock@gmail.com>
2021-09-15 20:35:13 +0900

    09-15-2030

--- express/board/models/index.js
+++ express/board/models/index.js
@@ -1,16 +1,14 @@
 const Sequelize = require("sequelize");
 
-const env = process.env.NODE_ENV || "developmet";
+const env = process.env.NODE_ENV || "development";
 
 const config = require("../config/config.json")[env];
-
 const sequelize = new Sequelize(
   config.database,
   config.username,
   config.password,
   config
 );
-
 const db = { Sequelize: Sequelize, sequelize };
 
 module.exports = db;
--- express/board/routes/member/memberindex.js
+++ express/board/routes/member/memberindex.js
@@ -1,3 +1,5 @@
+const member = require("../../models/member"); // 회원 모델
+const { joinValidator } = require("../../middlewares/middlewaresmember"); // 유효성검사 미들웨어
 const express = require("express");
 const router = express.Router();
 
@@ -15,7 +17,15 @@ router
     return res.render("member/form", data);
   })
   // 회원 가입 처리
-  .post((req, res) => {});
+  .post(joinValidator, async (req, res) => {
+    // const memNo = await member.join(req.body);
+    // if (memNo) {
+    //   //회원가입 성공
+    //   //로그인 처리 -> 메인페이지로 이동
+    // }
+    // 로그인 처리 실패 -> 메세지를 출력
+    res.send("");
+  });
 
 router
   .route("/login")
